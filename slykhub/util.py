7e43a2ecb55db44f8f4fedad9c924deaf1df9550
Abel Beldarrain <abeldarrainvasallo@gmail.com>
2023-02-21 13:55:02 -0500

    Backend returnung users list by date

--- slykhub/util.py
+++ slykhub/util.py
@@ -32,3 +32,32 @@ def convert(apikey,values, toasset, fromasset):
         rvalues.append(val)                 
     return rvalues
     
+def get_dict_user_growth(user_growth_dict, list_of_dates):
+    new_users_by_date_given = {}
+    for day in list_of_dates:
+        if user_growth_dict.get(day):
+            new_users_by_date_given[day] = user_growth_dict[day]
+        else:
+            new_users_by_date_given[day] = 0
+    return new_users_by_date_given
+
+def get_stacked_users_dict(user_growth_dict, list_of_dates):
+    total = 0
+    total_users_by_date_given = {}
+    ##########get stacked users til date##############
+    if list_of_dates[0] in list(user_growth_dict.keys()):
+        stop = list_of_dates[0]
+        for i in user_growth_dict.keys():
+            print(i)
+            if i == stop:
+                print('BBBBRRREEEEAAAAKKKK')
+                break
+            else:
+                total += user_growth_dict[i]
+    
+    ####make dict####
+    for day in list_of_dates:
+            total += user_growth_dict[day]
+            total_users_by_date_given[day] = total
+    
+    return total_users_by_date_given
